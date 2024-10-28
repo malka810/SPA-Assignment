c3cf632f3e3d7fd7dc9bec5c36fb8a80f8bb6e1f
Malka Samarakoon <malkasamarakoon30@gmail.com>
2024-10-28 11:03:56 +0530

    update

--- js/Script.js
+++ js/Script.js
@@ -1,64 +1,51 @@
-const loadingHome = document.querySelector('#Home');
-const loadingCustomer = document.querySelector('#Customer');
-const loadingItem = document.querySelector('#Item');
-const loadingOrder = document.querySelector('#Order');
-const loadingOrderDetails = document.querySelector('#orderDetails');
-const myFunction=function (i){
-    let id=['#home_page','#customer_page','#item_page','#order_page','#order_details_page'];
-    let loadingScreens=[loadingHome,loadingCustomer,loadingItem,loadingOrder,loadingOrderDetails];
-    for (let j = 0; j < id.length; j++) {
-        if (i===j){
-            loadingScreens[j].style.display = 'block';
-            $(id[j]).removeClass("gradient-text");
-            $(id[j]).addClass("active");
-        }else{
-            loadingScreens[j].style.display = 'none';
-            $(id[j]).removeClass("active");
-            $(id[j]).addClass("gradient-text");
-        }
-    }
-}
+  let home_page = document.getElementById("home_page");
+  let customer_page = document.getElementById("customer_page");
+  let item_page = document.getElementById("item_page");
+  let order_page = document.getElementById("order_page");
+  let order_details_page = document.getElementById("order_details_page");
 
-window.addEventListener('load', ()=>{
-    $("#preloader").css("display","none");
-    myFunction(0);
-});
+  let Home = document.getElementById("Home");
+  let Customer = document.getElementById("Customer");
+  let Item = document.getElementById("Item");
+  let Order = document.getElementById("Order");
+  let OrderDetails = document.getElementById("OrderDetails");
 
-$(document).ready(function () {
-    $('#home_page').on('click',  () => {
-        myFunction(0);
-    });
+  Customer.style.display="none";
+  Item.style.display="none";
+  Order.style.display="none";
+  OrderDetails.style.display="none";
 
-    $('#customer_page').on('click', function () {
-        myFunction(1);
-    });
+  customer_page.addEventListener('click', function () {
+    Home.style.display="none"
+    Customer.style.display="block";
+    Item.style.display="none";
+    Order.style.display="none";
+    OrderDetails.style.display="none";
+  });
 
-    $('#item_page').on('click', function () {
-        myFunction(2);
-    });
-
-    $('#order_page').on('click', function () {
-        myFunction(3);
-    });
-
-    $('#order_details_page').on('click', function () {
-        myFunction(4);
-    });
-});
-
-
-
-let customer_array = [];
-
-
-const loadCustomerTable = () => {
-    $("#customerTableBody").empty();
-    customer_array.map((item, index) => {
-        console.log(item);
-        let data = `<tr><td>${item.customer_id}</td><td>${item.name}</td><td>${item.address}</td><td>${item.contact}</td><td>${item.email}</td></tr>`
-        $("#customerTableBody").append(data);
-    })
-}
+  item_page.addEventListener('click', function () {
+    Home.style.display = "none";
+    Customer.style.display = "none";
+    Item.style.display = "block";
+    Order.style.display = "none";
+    OrderDetails.style.display = "none";
+  });
+  
+  order_page.addEventListener('click', function () {
+    Home.style.display = "none";
+    Customer.style.display = "none";
+    Item.style.display = "none";
+    Order.style.display = "block";
+    OrderDetails.style.display = "none";
+  });
+  
+  order_details_page.addEventListener('click', function () {
+    Home.style.display = "none";
+    Customer.style.display = "none";
+    Item.style.display = "none";
+    Order.style.display = "none";
+    OrderDetails.style.display = "block";
+  });
 
 $("#customer_add_btn").on("click", function() {
     let customer_id = $('#customer_id').val();
